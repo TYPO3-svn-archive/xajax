4c383477f6df469e7a34e6e87e24349404f6e1b8
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-09 19:12:15 +0000

    Branch xajax Multi Plugin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xajax/branches/multiplugin@5894 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "xajaxmp"
+#
+# Auto generated 15-06-2007 21:38
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'xajax',
+	'description' => 'The handy xajax 0.2.4 library: xajax is a PHP class library that allows you to easily create powerful, web-based, Ajax applications using HTML, CSS, JavaScript, and PHP. You find the documentation within it\'s own extension with the key xajax_tutor.',
+	'category' => 'misc',
+	'author' => 'Elmar Hinz, Jared White, J. Max Wilson, Ricardo Mieres (rmieres@marketing-factory.de)',
+	'author_email' => 'elmar.hinz@team-red.net',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.2.4',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"56a8";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"35f7";s:20:"doc/wizard_form.html";s:4:"930f";}',
+);
+
+?>
