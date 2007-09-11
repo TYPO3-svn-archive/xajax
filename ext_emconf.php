94ce2b88341847332920005b75eacc202e7899a9
derjoerg <derjoerg@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-09-11 12:27:21 +0000

    Update from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xajax/branches/0.x@6493 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "xajax"
 #
-# Auto generated 28-12-2006 00:33
+# Auto generated 11-09-2007 14:27
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'The handy xajax 0.2.4 library: xajax is a PHP class library that allows you to easily create powerful, web-based, Ajax applications using HTML, CSS, JavaScript, and PHP. You find the documentation within it\'s own extension with the key xajax_tutor.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.2.4',
+	'version' => '0.2.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:74:{s:9:"ChangeLog";s:4:"16c7";s:11:"LICENSE.txt";s:4:"0cce";s:10:"README.txt";s:4:"c43a";s:18:"class.tx_xajax.php";s:4:"1438";s:27:"class.tx_xajax_response.php";s:4:"9fbd";s:12:"ext_icon.gif";s:4:"3ec5";s:8:"icon.gif";s:4:"4aa7";s:17:"xajaxCompress.php";s:4:"9399";s:14:"doc/manual.sxw";s:4:"8155";s:19:"doc/.svn/README.txt";s:4:"cf1a";s:20:"doc/.svn/dir-wcprops";s:4:"bc27";s:19:"doc/.svn/empty-file";s:4:"d41d";s:16:"doc/.svn/entries";s:4:"aee3";s:15:"doc/.svn/format";s:4:"48a2";s:34:"doc/.svn/props/manual.sxw.svn-work";s:4:"c5ac";s:38:"doc/.svn/text-base/manual.sxw.svn-base";s:4:"8155";s:38:"doc/.svn/prop-base/manual.sxw.svn-base";s:4:"c5ac";s:36:"doc/.svn/wcprops/manual.sxw.svn-work";s:4:"9848";s:15:".svn/README.txt";s:4:"cf1a";s:16:".svn/dir-wcprops";s:4:"9764";s:15:".svn/empty-file";s:4:"d41d";s:12:".svn/entries";s:4:"83b4";s:11:".svn/format";s:4:"48a2";s:29:".svn/props/ChangeLog.svn-work";s:4:"729c";s:31:".svn/props/LICENSE.txt.svn-work";s:4:"729c";s:30:".svn/props/README.txt.svn-work";s:4:"729c";s:38:".svn/props/class.tx_xajax.php.svn-work";s:4:"729c";s:47:".svn/props/class.tx_xajax_response.php.svn-work";s:4:"729c";s:34:".svn/props/ext_emconf.php.svn-work";s:4:"729c";s:32:".svn/props/ext_icon.gif.svn-work";s:4:"545b";s:28:".svn/props/icon.gif.svn-work";s:4:"545b";s:37:".svn/props/xajaxCompress.php.svn-work";s:4:"729c";s:33:".svn/text-base/ChangeLog.svn-base";s:4:"1bfc";s:35:".svn/text-base/LICENSE.txt.svn-base";s:4:"0cce";s:34:".svn/text-base/README.txt.svn-base";s:4:"c43a";s:42:".svn/text-base/class.tx_xajax.php.svn-base";s:4:"11bf";s:51:".svn/text-base/class.tx_xajax_response.php.svn-base";s:4:"9fbd";s:38:".svn/text-base/ext_emconf.php.svn-base";s:4:"f1f5";s:36:".svn/text-base/ext_icon.gif.svn-base";s:4:"3ec5";s:32:".svn/text-base/icon.gif.svn-base";s:4:"4aa7";s:41:".svn/text-base/xajaxCompress.php.svn-base";s:4:"9399";s:33:".svn/prop-base/ChangeLog.svn-base";s:4:"729c";s:35:".svn/prop-base/LICENSE.txt.svn-base";s:4:"729c";s:34:".svn/prop-base/README.txt.svn-base";s:4:"729c";s:42:".svn/prop-base/class.tx_xajax.php.svn-base";s:4:"729c";s:51:".svn/prop-base/class.tx_xajax_response.php.svn-base";s:4:"729c";s:38:".svn/prop-base/ext_emconf.php.svn-base";s:4:"729c";s:36:".svn/prop-base/ext_icon.gif.svn-base";s:4:"545b";s:32:".svn/prop-base/icon.gif.svn-base";s:4:"545b";s:41:".svn/prop-base/xajaxCompress.php.svn-base";s:4:"729c";s:31:".svn/wcprops/ChangeLog.svn-work";s:4:"4790";s:33:".svn/wcprops/LICENSE.txt.svn-work";s:4:"7d55";s:32:".svn/wcprops/README.txt.svn-work";s:4:"999e";s:40:".svn/wcprops/class.tx_xajax.php.svn-work";s:4:"7ab4";s:49:".svn/wcprops/class.tx_xajax_response.php.svn-work";s:4:"f758";s:36:".svn/wcprops/ext_emconf.php.svn-work";s:4:"c5ab";s:34:".svn/wcprops/ext_icon.gif.svn-work";s:4:"15a8";s:30:".svn/wcprops/icon.gif.svn-work";s:4:"196a";s:39:".svn/wcprops/xajaxCompress.php.svn-work";s:4:"a03a";s:17:"xajax_js/xajax.js";s:4:"ac78";s:30:"xajax_js/xajax_uncompressed.js";s:4:"6f80";s:24:"xajax_js/.svn/README.txt";s:4:"cf1a";s:25:"xajax_js/.svn/dir-wcprops";s:4:"c660";s:24:"xajax_js/.svn/empty-file";s:4:"d41d";s:21:"xajax_js/.svn/entries";s:4:"9dc3";s:20:"xajax_js/.svn/format";s:4:"48a2";s:37:"xajax_js/.svn/props/xajax.js.svn-work";s:4:"729c";s:50:"xajax_js/.svn/props/xajax_uncompressed.js.svn-work";s:4:"729c";s:41:"xajax_js/.svn/text-base/xajax.js.svn-base";s:4:"ac78";s:54:"xajax_js/.svn/text-base/xajax_uncompressed.js.svn-base";s:4:"6f80";s:41:"xajax_js/.svn/prop-base/xajax.js.svn-base";s:4:"729c";s:54:"xajax_js/.svn/prop-base/xajax_uncompressed.js.svn-base";s:4:"729c";s:39:"xajax_js/.svn/wcprops/xajax.js.svn-work";s:4:"78a6";s:52:"xajax_js/.svn/wcprops/xajax_uncompressed.js.svn-work";s:4:"1f75";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"28dd";s:11:"LICENSE.txt";s:4:"0cce";s:10:"README.txt";s:4:"13d9";s:18:"class.tx_xajax.php";s:4:"a4f1";s:27:"class.tx_xajax_response.php";s:4:"f990";s:12:"ext_icon.gif";s:4:"3ec5";s:8:"icon.gif";s:4:"4aa7";s:17:"xajaxCompress.php";s:4:"9399";s:14:"doc/manual.sxw";s:4:"8155";s:17:"xajax_js/xajax.js";s:4:"0b50";s:30:"xajax_js/xajax_uncompressed.js";s:4:"8e5d";}',
 	'suggests' => array(
 	),
 );
